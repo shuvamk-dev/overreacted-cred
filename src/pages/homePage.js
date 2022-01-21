f3c982a3bdea4a45949621bedfdf879a35cfdaa0
shuvamk <kshuvam564@gmail.com>
2022-01-21 23:37:08 +0530

    Created brands love and window peak

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import MobileScroll from '../components/MobileScroll';
+import React from "react";
+import BrandsLove from "../components/BrandsLove";
+import MobileScroll from "../components/MobileScroll";
+import WindowPeak from "../components/WindowPeak";
 
 const HomePage = () => {
-  return <MobileScroll />
+  return (
+    <>
+      <BrandsLove />
+      <MobileScroll />
+      <div className="non-mobile">
+        <WindowPeak />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
