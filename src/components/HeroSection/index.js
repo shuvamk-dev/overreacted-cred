d10d4258b26122c1ae5b0bb60551db7752f80c6c
shuvamk <kshuvam564@gmail.com>
2022-01-22 01:16:13 +0530

    Added claim label

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -4,12 +4,26 @@ import "./heroSection.css";
 
 const HeroSection = () => {
   return (
-    <div className="flex flex-col absolute-center hero-section max-width">
-      <div className="hero-heading">rewards for paying credit card bills.</div>
-      <div className="hero-subheading">
-        join 7.5M+ members who win rewards and cashbacks everyday
+    <div className="hero-section-wrapper">
+      <div className="flex absolute-center hero-claim-label">
+        <div>pay credit card bill. earn guaranteed ₹200 back.</div>
+        <div className="claim-anchor">
+          claim now
+          <img
+            src="https://web-images.credcdn.in/_next/assets/images/home-page/arrow.png"
+            className="claim-arrow"
+          />
+        </div>
       </div>
-      <Button buttonText="Download CRED" />
+      <div className="flex flex-col absolute-center hero-section max-width">
+        <div className="hero-heading">
+          rewards for paying credit card bills.
+        </div>
+        <div className="hero-subheading">
+          join 7.5M+ members who win rewards and cashbacks everyday
+        </div>
+        <Button buttonText="Download CRED" />
+      </div>
     </div>
   );
 };
