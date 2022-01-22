03098f98571399464b34c70fd2d76263d237cc76
shuvamk <kshuvam564@gmail.com>
2022-01-22 14:21:49 +0530

    Creted header for mobile

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
+  const [showMobMenu, setShowMobMenu] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setShowMobMenu(!showMobMenu);
+    document.querySelector("body").classList.toggle("body-overflow-visible");
+  };
+
   return (
-    <div className="max-width flex header">
-      <img
-        src="https://web-images.credcdn.in/_next/assets/images/home-page/cred-logo.png"
-        className="header-logo"
-      />
-      <div className="only-mobile"></div>
-      <div className="non-mobile flex">
-        <div className="header-nav-item">credit score check</div>
-        <div className="header-nav-item">credit card bill payment</div>
+    <div className="mobile-menu-wrapper">
+      <div
+        className={`mobile-menu only-mobile ${showMobMenu ? "overlay" : ""}`}
+      >
+        <div className="mobile-navbar">
+          <div className="mobile-nav-item">credit score check</div>
+          <div className="mobile-nav-item">credit card bill payment</div>
+        </div>
+      </div>
+      <div className="max-width flex header">
+        <img
+          src="https://web-images.credcdn.in/_next/assets/images/home-page/cred-logo.png"
+          className="header-logo"
+        />
+        <div className="only-mobile mobile-menu-button-wrapper">
+          <button
+            class={`hamburger hamburger--spin ${
+              showMobMenu ? "is-active" : ""
+            }`}
+            type="button"
+            onClick={toggleMobileMenu}
+          >
+            <span class="hamburger-box">
+              <span class="hamburger-inner"></span>
+            </span>
+          </button>
+        </div>
+        <div className="non-mobile flex">
+          <div className="header-nav-item">credit score check</div>
+          <div className="header-nav-item">credit card bill payment</div>
+        </div>
       </div>
     </div>
   );
